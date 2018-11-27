ca81d4de62066bcda1c05a48480cd358196c20c9
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-26 16:51:17 -0800

    tweak existing HelloWorld component, delete logo, add synth.js data

---
+++ synths.js
@@ -0,0 +1,22 @@
+const synths = [
+    {
+        name: 'Korg MS-20',
+        image: 'http://www.vintagesynth.com/sites/default/files/2017-05/ms20.jpg',
+        polyphony: 'monophonic',
+        year: '1978'
+    },
+    {
+        name: 'Octave The Cat',
+        image: 'http://www.vintagesynth.com/sites/default/files/2017-05/octave_cat_angle.jpg',
+        polyphony: 'duophonic',
+        year: '1976'
+    },
+    {
+        name: 'Roland Juno-6',
+        image: 'http://www.vintagesynth.com/sites/default/files/2017-05/roland_juno6_angle.jpg',
+        polyphony: 'polyphonic',
+        year: '1982'
+    }
+];
+
+export default synths;
