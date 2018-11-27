33793cdd237b7bd6ce656dee1c84bac3af8e5e53
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-26 17:03:57 -0800

    added synth.js to components folder, hooked to App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <!--<img alt="Vue logo" src="./assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Synths/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import Synths from './components/Synths.vue';
 
 export default {
-    name: 'app',
+//    name: 'app',
     components: {
-        HelloWorld
+        Synths
     }
 };
 </script>
---
+++ src/components/Synths.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li>
+            <h3>Synth Name</h3>
+            <img src="http://www.vintagesynth.com/sites/default/files/2017-05/ms20.jpg">
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+li {
+    display: flex;
+}
+
+</style>
