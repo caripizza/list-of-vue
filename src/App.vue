24cc862d46c2d04b683dc229a8dddcca0c63b861
Cari Palazzolo <cari.pizza@gmail.com>
2018-11-26 17:30:52 -0800

    added directives to Synth.vue for onSelect li

--- src/App.vue
+++ src/App.vue
@@ -26,11 +26,8 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: purple;
 }
 </style>
--- src/components/Synths.vue
+++ src/components/Synths.vue
@@ -2,7 +2,8 @@
     <ul>
         <li v-for="synth in synths"
             v-bind:key="synth.name"
-        >
+            @click="onSelect(synth)"
+            >
             <h3>{{synth.name}}</h3>
             <img v-bind:src="synth.image">
         </li>
@@ -18,8 +19,21 @@ export default {
 </script>
 
 <style>
+ul {
+    list-style: none;
+    padding-left: 0px;
+}
 li {
     display: flex;
+    flex-direction: column;
+    text-align: center;
+    border: 2px solid purple;
+    cursor: pointer;
+}
+li img {
+    width: 25%;
+    height: 25%;
+    margin: 0 auto;
 }
 
 </style>
