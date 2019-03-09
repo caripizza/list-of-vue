7be6d3a36ee25506577c0aa87abdc9e655efa41f
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-09 10:29:49 -0800

    use local image assets

--- src/synths.js
+++ src/synths.js
@@ -1,7 +1,11 @@
+import ms20 from '../public/ms20.jpg';
+import kitten from '../public/kitten.jpg';
+import rolandjuno6 from '../public/rolandjuno6.jpg';
+
 const synths = [
     {
         name: 'Korg MS-20',
-        image: 'http://www.vintagesynth.com/sites/default/files/2017-05/ms20.jpg',
+        image: ms20,
         polyphony: 'monophonic',
         year: '1978',
         href: 'http://www.vintagesynth.com/korg/ms20.php'
@@ -9,14 +13,14 @@ const synths = [
     },
     {
         name: 'Octave The Kitten',
-        image: 'http://www.vintagesynth.com/sites/default/files/2017-05/kitten.jpg',
+        image: kitten,
         polyphony: 'monophonic',
         year: '1981',
         href: 'http://www.vintagesynth.com/misc/octavekit.php'
     },
     {
         name: 'Roland Juno-6',
-        image: 'http://www.vintagesynth.com/sites/default/files/2017-05/roland_juno6_angle.jpg',
+        image: rolandjuno6,
         polyphony: 'polyphonic',
         year: '1982',
         href: 'http://www.vintagesynth.com/roland/juno6.php'
